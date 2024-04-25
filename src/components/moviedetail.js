82a6f3c333e7bf51065305e986e3de3134455141
Yeabsera <yeabbert@gmail.com>
2024-04-25 16:27:36 -0600

    idek

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -48,20 +48,20 @@ class MovieDetail extends Component {
                         <ListGroup>
                             <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
                             <ListGroupItem>
-                                {this.props.selectedMovie.actors.map((actor, i) =>
+                                {this.props.selectedMovie.actors.map((actor, i) => (
                                     <p key={i}>
                                         <b>{actor.actorName}</b> {actor.characterName}
-                                    </p>)}
+                                    </p>))}
                             </ListGroupItem>
                             <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
                         </ListGroup>
                         <Card.Body>
-                            {this.props.selectedMovie.reviews &&this.props.selectedMovie.reviews.map((review, i) =>
+                            {this.props.selectedMovie.movieReviews &&this.props.selectedMovie.movieReviews.map((review, i) => (
                                 <p key={i}>
                                     <b>{review.username}</b>&nbsp; {review.review}
                                     &nbsp;  <BsStarFill/> {review.rating}
                                 </p>
-                            )}
+                            ))}
                         </Card.Body>
                     </Card>
                 )
