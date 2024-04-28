cf3a64c580a58440ede74e4b91a3c4153fa1d79e
Yeabsera <yeabbert@gmail.com>
2024-04-27 22:26:27 -0600

    idek

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -38,7 +38,7 @@ class MovieDetail extends Component {
     handleReviewSubmit = (event) => {
         event.preventDefault();
         const {dispatch} = this.props;
-        dispatch(setReview(movieId, this.state.review));
+        dispatch(setReview(this.props.movieId, this.state.review));
 
     }
 
